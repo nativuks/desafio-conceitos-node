a0acde87919de6720e85e257930b402d12529282
Luis sejas <nativuks@gmail.com>
2020-08-26 17:34:28 -0400

    List repositories

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
